5aca3d9f2271c18f3c274f77e99d45d77c6c2f9f
rfx <retrofreak138@gmail.com>
2021-04-08 01:09:59 +0200

    Fix missing data on match page

--- js/match.js
+++ js/match.js
@@ -3,14 +3,15 @@ var infoRequest = new XMLHttpRequest();
 infoRequest.onload = function () {
     var data = JSON.parse(this.response)
     document.title = "MadBomber.NET - Match#" + data.id;
-    $("#mbf-match-id").html(data.id);
+    $("#mbf-match-title").html("Match#" + data.id);
+    $("#mbf-match-timestamp").html(data.timestamp);
     $("#mbf-match-playtime").html(data.playtime + " sec.");
     $("#mbf-match-winner").html('<a href="/player.html?id=' + data.winner_id + '">' + data.winner_name + '</a>');
     $("#mbf-match-map").html('<a href="/map.html?id=' + data.map_id + '">' + data.map_name + '</a>');
     $("#mbf-match-gametype").html('<a href="/gametype.html?id=' + data.game_type_id + '">' + data.game_type_name + '</a>');
     $("#mbf-match-server").html('<a href="/server.html?id=' + data.server_id + '">' + data.server_name + '</a>');
     counter = 0;
-    data.forEach((kill) => {
+    data.kills.forEach((kill) => {
         counter++;
         $('#mbf-match-kills').html(
             '<tr><td>' + 
